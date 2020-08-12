23b4a7c4247f79c0e05539ce1c3897d2af3bd318
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-12 15:10:36 -0700

    update user index controller to abstract current user from index list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,24 @@
 class UsersController < ApplicationController
+
   before_action :authenticate_user!
 
+
+
   def index
-    @users = User.all
+
+    @users = User.where.not(email: current_user.email)
+
   end
 
+
+
   def show
+
     @user = User.find(params[:id])
+
     @posts = @user.posts.ordered_by_most_recent
+
   end
+
 end
+
