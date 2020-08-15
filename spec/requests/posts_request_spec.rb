69d1088e6fe3abdd414205976d3cee31ad177a0e
Uduak Essien <acushla4real@gmail.com>
2020-08-15 03:36:58 +0100

    add test to create a post

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,11 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
+    include Devise::Test::IntegrationHelpers
 
     describe 'GET #index' do
         before { get posts_path }
     
         it { should redirect_to(new_user_session_path) }
-      end
+    end
+
+    describe 'Authenticated Users Activities' do
+        before { @user = User.create(name: 'User1', email: 'user1@gmail.com', password: 'password')
+                 sign_in @user }
+    
+        it 'should successfully access post timeline' do
+            get posts_path
+            assert_response :success
+        end
+
+        it 'should be able to create a post' do
+            @post = @user.posts.new(content: 'test post')
+
+            expect(@post.save).to eq(true)
+        end
+    end
 
 end
