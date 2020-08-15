e53fad633b7a344af6948a6bf79148ec9a49cebf
Uduak Essien <acushla4real@gmail.com>
2020-08-15 09:13:40 +0100

    fix rubocop lint errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,12 +4,10 @@ RSpec.describe Comment, type: :model do
   context 'validation test' do
     it { should validate_presence_of(:content) }
 
-
     it do
       should validate_length_of(:content)
         .is_at_most(200)
         .with_message('200 characters in comment is the maximum allowed.')
     end
-
   end
 end
