798e94a89b5d7c0ce3712ae4cd45c00ac000e610
Uduak Essien <acushla4real@gmail.com>
2020-08-15 03:08:34 +0100

    add test for log in users

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
+
 RSpec.describe "Users", type: :request do
-    it 'redirected if not logged in ' do
+    include Devise::Test::IntegrationHelpers
+
+    it 'should redirect if not logged in ' do
         get users_path
         assert_response :redirect
+    end
+
+    it 'should login' do
+        @user = User.create(name: 'User1', email: 'user1@gmail.com', password: 'password')
+        sign_in @user
+        get users_path
+        assert_response :success
     end
 end
