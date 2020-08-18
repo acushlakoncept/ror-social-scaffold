204ebe5b59417b113071e37e1bc062950c490fbe
Uduak Essien <acushla4real@gmail.com>
2020-08-18 10:20:15 +0100

    update invite_or_pending_btn

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,15 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+
   has_many :confirmed_friendships, -> { where status: true }, class_name: "Friendship"
   has_many :friends, through: :confirmed_friendships
+
   has_many :pending_friendships, -> { where status: false }, class_name: "Friendship", foreign_key: "user_id"
   has_many :pending_friends, through: :pending_friendships, source: :friend
 
-  has_many :inverted_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "friend_id"
-  has_many :friend_requests, through: :inverted_friendships
+  has_many :inverted_friendships, -> { where status: false }, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :friend_requests, through: :inverted_friendships, source: :user
 
   # def friends
   #   friends_array = friendships.map { |friendship| friendship.friend if friendship.status } +
@@ -47,10 +49,8 @@ class User < ApplicationRecord
   end
 
   def friends_and_own_posts
-    Post.where(user: (self.friends + self))
+    Post.where('user_id IN (?)', friends_ids)
   end
-
-
 
   def reject_request(user)
     friendship = inverse_friendships.find { |f| f.user == user }
@@ -58,15 +58,15 @@ class User < ApplicationRecord
   end
 
   def friend?(user)
-    friends.include?(user)
+    self.friends.include?(user)
   end
 
   def mutual_friends(user)
     friends & user.friends
   end
 
-  # def friends_ids
-  #   f_ids = friends.map(&:id)
-  #   f_ids << id
-  # end
+  def friends_ids
+    f_ids = self.friends.map(&:id)
+    f_ids << self.id
+  end
 end
