3543679be07f0506763446a704c2c2896cdace44
Uduak Essien <acushla4real@gmail.com>
2020-08-18 11:26:33 +0100

    fix rubocop lint error

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,21 +12,20 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
-  has_many :confirmed_friendships, -> { where status: true }, class_name: "Friendship"
+  has_many :confirmed_friendships, -> { where status: true }, class_name: 'Friendship'
   has_many :friends, through: :confirmed_friendships
 
-  has_many :pending_friendships, -> { where status: false }, class_name: "Friendship", foreign_key: "user_id"
+  has_many :pending_friendships, -> { where status: false }, class_name: 'Friendship', foreign_key: 'user_id'
   has_many :pending_friends, through: :pending_friendships, source: :friend
 
-  has_many :inverted_friendships, -> { where status: false }, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :inverted_friendships, -> { where status: false }, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :friend_requests, through: :inverted_friendships, source: :user
 
-
   def confirm_friend(user)
-    friend = Friendship.find_by(user_id: user.id, friend_id: self.id)
+    friend = Friendship.find_by(user_id: user.id, friend_id: id)
     friend.status = true
     friend.save
-    Friendship.create!(friend_id: user.id, user_id: self.id, status: true)
+    Friendship.create!(friend_id: user.id, user_id: id, status: true)
   end
 
   def friends_and_own_posts
@@ -39,15 +38,15 @@ class User < ApplicationRecord
   end
 
   def friend?(user)
-    self.friends.include?(user)
+    friends.include?(user)
   end
 
   def mutual_friends(user)
-    friends & user.friends
+    self.friends & user.friends
   end
 
   def friends_ids
-    f_ids = self.friends.map(&:id)
-    f_ids << self.id
+    f_ids = friends.map(&:id)
+    f_ids << id
   end
 end
