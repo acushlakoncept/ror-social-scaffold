3d5eb1da069ade97a757b5dc7583b582e71405b3
Uduak Essien <acushla4real@gmail.com>
2020-08-19 11:35:27 +0100

    add end to end testing for accessing all users page

--- spec/features/invite_friend_spec.rb
+++ spec/features/invite_friend_spec.rb
@@ -3,15 +3,16 @@ require 'rails_helper'
   describe "the signin process", :type => :feature do
     before :each do
       User.create(email: 'user1@gmail.com', password: 'password', name: 'User1')
+      visit '/users/sign_in'
+      fill_in 'Email', :with => 'user1@gmail.com'
+      fill_in 'Password', :with => 'password'
+      click_button 'Log in'
     end
   
     it "signs @user in" do
-        visit '/users/sign_in'
-        fill_in 'Email', :with => 'user1@gmail.com'
-        fill_in 'Password', :with => 'password'
-        click_button 'Log in'
-        expect(current_path).to eq(root_path)
-        expect(page).to have_text('Signed in successfully.')
+        visit users_path
+        expect(page).to have_text('User1') 
+        expect(page).to have_link('See Profile')
     end
 
     it "does not sign in with wrong info" do
